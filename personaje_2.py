e8a92d81f6e4fc03073c08f0ff774724d7edc5b6
Nfrancoo <francotobiasquiro@gmail.com>
2023-06-23 17:08:53 -0300

    mi game

--- personaje_2.py
+++ personaje_2.py
@@ -21,7 +21,7 @@ class Personaje():
     self.cooldown_ataque = 0
     self.sonido_ataque = sound
     self.hit = False
-    self.vida = 10000
+    self.vida = 100
     self.vivo = True
     self.rango_ataque = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
 
